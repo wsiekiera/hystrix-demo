7dc2e4a7fc28765d6bf238fe052dcf2f706d6375
Wojciech Siekiera <wojciech@siekiera.net>
2019-06-14 21:26:37 +0200

    initial commit

---
+++ hystrix-rest-server/src/main/java/net/siekiera/hystrixrestserver/HystrixRestServerApplication.java
@@ -0,0 +1,13 @@
+package net.siekiera.hystrixrestserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixRestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixRestServerApplication.class, args);
+	}
+
+}
